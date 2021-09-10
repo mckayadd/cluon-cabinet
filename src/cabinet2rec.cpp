8554cf8266bc3e480b0580f94c5e3e84b7c0e935
Christian Berger <christian.berger@gu.se>
2021-09-10 23:22:23 +0200

    * cabinet2rec dumps the content of the cabinet back to a .rec file
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ src/cabinet2rec.cpp
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2021  Christian Berger
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "cluon-complete.hpp"
+#include "lmdb.h"
+
+#include <cstdio>
+#include <cstring>
+#include <cstdint>
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+
+typedef unsigned __int128 uint128_t;
+
+int32_t main(int32_t argc, char **argv) {
+  int32_t retCode{0};
+  auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+  if (0 == commandlineArguments.count("cabinet")) {
+    std::cerr << argv[0] << " exports all entries from the 'all' table of a cabinet (an lmdb-based key/value-database) as Envelopes to a .rec-file." << std::endl;
+    std::cerr << "Usage:   " << argv[0] << " --cabinet=myStore.cabinet" << std::endl;
+    std::cerr << "         --cabinet: name of the database file" << std::endl;
+    std::cerr << "         --rec:     name of the rec file (optional; otherwise, a new file based on the .cabinet file with .rec as suffix is created)" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --cabinet=myStore.cabinet --rec=myRecFile.rec" << std::endl;
+    retCode = 1;
+  } else {
+    const std::string CABINET{commandlineArguments["cabinet"]};
+    const std::string REC{(commandlineArguments["rec"].size() != 0) ? commandlineArguments["rec"] : "./" + CABINET + ".rec"};
+
+    MDB_env *env{nullptr};
+    const int numberOfDatabases{100};
+    const int64_t SIZE_120TB = 120UL * 1024UL * 1024UL * 1024UL * 1024UL;
+
+    // lambda to check the interaction with the database.
+    auto checkErrorCode = [_argv=argv](int32_t rc, int32_t line, std::string caller) {
+      if (0 != rc) {
+        std::cerr << "[" << _argv[0] << "]: " << caller << ", line " << line << ": (" << rc << ") " << mdb_strerror(rc) << std::endl; 
+      }
+      return (0 == rc);
+    };
+
+    if (!checkErrorCode(mdb_env_create(&env), __LINE__, "mdb_env_create")) {
+      return 1;
+    }
+    if (!checkErrorCode(mdb_env_set_maxdbs(env, numberOfDatabases), __LINE__, "mdb_env_set_maxdbs")) {
+      mdb_env_close(env);
+      return 1;
+    }
+    if (!checkErrorCode(mdb_env_set_mapsize(env, SIZE_120TB), __LINE__, "mdb_env_set_mapsize")) {
+      mdb_env_close(env);
+      return 1;
+    }
+    if (!checkErrorCode(mdb_env_open(env, CABINET.c_str(), MDB_NOSUBDIR|MDB_RDONLY, 0600), __LINE__, "mdb_env_open")) {
+      mdb_env_close(env);
+      return 1;
+    }
+
+    std::fstream recFile;
+    recFile.open(REC.c_str(), std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
+    if (recFile.good()) {
+      MDB_txn *txn{nullptr};
+      MDB_dbi dbi{0};
+      if (!checkErrorCode(mdb_txn_begin(env, nullptr, MDB_RDONLY, &txn), __LINE__, "mdb_txn_begin")) {
+        mdb_env_close(env);
+        return (retCode = 1);
+      }
+      retCode = mdb_dbi_open(txn, "all", 0 , &dbi);
+      if (MDB_NOTFOUND  == retCode) {
+        std::clog << "[" << argv[0] << "]: No database 'all' found in " << CABINET << "." << std::endl;
+      }
+      else {
+        uint64_t numberOfEntries{0};
+        MDB_stat stat;
+        if (!mdb_stat(txn, dbi, &stat)) {
+          numberOfEntries = stat.ms_entries;
+        }
+        std::clog << "[" << argv[0] << "]: Found " << numberOfEntries << " entries in database 'all' in " << CABINET << std::endl;
+
+        MDB_cursor *cursor;
+        if (!(retCode = mdb_cursor_open(txn, dbi, &cursor))) {
+          uint64_t entries{0};
+          int32_t oldPercentage{-1};
+          MDB_val key;
+          MDB_val val;
+          while ((retCode = mdb_cursor_get(cursor, &key, &val, MDB_NEXT_NODUP)) == 0) {
+            recFile.write(static_cast<char*>(val.mv_data), val.mv_size);
+
+            entries++;
+            const int32_t percentage = static_cast<int32_t>(static_cast<float>(entries * 100.0f) / static_cast<float>(numberOfEntries));
+            if ((percentage % 5 == 0) && (percentage != oldPercentage)) {
+              std::clog << "[" << argv[0] << "]: Processed " << percentage << "% (" << entries << " entries) from " << CABINET << "." << std::endl;
+              oldPercentage = percentage;
+              recFile.flush();
+            }
+          }
+          recFile.close();
+          mdb_cursor_close(cursor);
+        }
+      }
+      mdb_txn_abort(txn);
+      if (dbi) {
+        mdb_dbi_close(env, dbi);
+      }
+    }
+    else {
+      std::cerr << "[" << argv[0] << "]: Error opening " << REC << std::endl; 
+    }
+    if (env) {
+      mdb_env_close(env);
+    }
+  }
+  return retCode;
+}
