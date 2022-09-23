e5b142624a737aa06738aed9fee2c0545abe8b85
Christian Berger <christian.berger@gu.se>
2022-09-23 20:47:27 +0000

    added start/end and export filter to cabinet-stream
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- src/cabinet-stream.cpp
+++ src/cabinet-stream.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021  Christian Berger
+ * Copyright (C) 2021,2022  Christian Berger
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -23,10 +23,13 @@ int32_t main(int32_t argc, char **argv) {
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
   if (0 == commandlineArguments.count("cab")) {
     std::cerr << argv[0] << " prints all entries from table 'all' of a cabinet (an lmdb-based key/value-database) as Envelopes to stdout." << std::endl;
-    std::cerr << "Usage:   " << argv[0] << " --cab=myStore.cab [--mem=32024] [--verbose]" << std::endl;
+    std::cerr << "Usage:   " << argv[0] << " --cab=myStore.cab [--mem=32024] [--verbose] [--start=1569916731] [--end=+100] [--export=19/0,25/1]" << std::endl;
     std::cerr << "         --cab:     name of the database file" << std::endl;
     std::cerr << "         --mem:     upper memory size for database in memory in GB, default: 64,000 (representing 64TB)" << std::endl;
     std::cerr << "         --verbose: display information on stderr" << std::endl;
+    std::cerr << "         --start:   start time stamp in Unix epoch seconds (export begins AFTER this time point)" << std::endl;
+    std::cerr << "         --end:     end time stamp in Unix epoch seconds; or +duration in seconds (export ends BEFORE this time point)" << std::endl;
+    std::cerr << "         --export:  <list of messageID/senderStamp pairs to export, default: all>" << std::endl;
     std::cerr << "Example: " << argv[0] << " --cab=myStore.cab" << std::endl;
     retCode = 1;
   } else {
@@ -36,8 +39,37 @@ int32_t main(int32_t argc, char **argv) {
     const uint64_t MEM{(commandlineArguments["mem"].size() != 0) ? static_cast<uint64_t>(std::stoi(commandlineArguments["mem"])) : 64UL*1024UL};
     const bool VERBOSE{(commandlineArguments["verbose"].size() != 0)};
 
+    std::map<std::string, bool> mapOfEnvelopesToExport{};
+    {
+      std::string tmp{commandlineArguments["export"]};
+      tmp += ",";
+      auto entries = stringtoolbox::split(tmp, ',');
+      for (auto e : entries) {
+        // Use only valid entries.
+        if (0 != e.size()) {
+          auto l = stringtoolbox::split(e, '/');
+          std::string toKeep{e};
+          if (0 == l.size()) {
+              toKeep += "/0";
+          }
+          std::cerr << argv[0] << " keeping " << toKeep << std::endl;
+          mapOfEnvelopesToExport[toKeep] = true;
+        }
+      }
+    }
+
+    const int32_t START{(commandlineArguments.count("start") != 0) ? static_cast<int32_t>(std::stoi(commandlineArguments["start"])) : 0};
+    int32_t END{(commandlineArguments.count("end") != 0) ? static_cast<int32_t>(std::stoi(commandlineArguments["end"])) : (std::numeric_limits<int32_t>::max)()};
+    if (commandlineArguments.count("end") != 0) {
+        std::string END_TMP = commandlineArguments["end"];
+        if (END_TMP.at(0) == '+') {
+          // relative end notation was used.
+          END += START;
+        }
+    }
+
     const std::string ARGV0{argv[0]};
-    retCode = cabinet_stream(ARGV0, MEM, CABINET, VERBOSE);
+    retCode = cabinet_stream(ARGV0, MEM, CABINET, VERBOSE, mapOfEnvelopesToExport, START, END);
   }
   return retCode;
 }
--- src/cabinet-stream.hpp
+++ src/cabinet-stream.hpp
@@ -13,6 +13,8 @@
 #include "db.hpp"
 #include "key.hpp"
 #include "lmdb.h"
+#include "lz4.h"
+#include "lz4hc.h"
 
 #include <cstdio>
 #include <cstring>
@@ -31,13 +33,17 @@
  * @param ARGV0 Our name.
  * @param CABINET The file to replay.
  * @param VERBOSE
+ * @param 
  * @return 0 on success, 1 otherwise
  */
-inline int cabinet_stream(const std::string &ARGV0, const uint64_t &MEM, const std::string &CABINET, const bool &VERBOSE) {
+inline int cabinet_stream(const std::string &ARGV0, const uint64_t &MEM, const std::string &CABINET, const bool &VERBOSE, const std::map<std::string, bool> &mapOfEnvelopesToExport, const int32_t &START, const int32_t &END) {
   int32_t retCode{0};
+  uint64_t entries{0};
   MDB_env *env{nullptr};
   const int numberOfDatabases{100};
   const int64_t SIZE_DB = MEM * 1024UL * 1024UL * 1024UL;
+  const int64_t START_IN_NS = START * 1000UL * 1000UL * 1000UL;
+  const int64_t END_IN_NS = END * 1000UL * 1000UL * 1000UL;
 
   // lambda to check the interaction with the database.
   auto checkErrorCode = [argv0=ARGV0, VERBOSE](int32_t rc, int32_t line, std::string caller) {
@@ -87,27 +93,70 @@ inline int cabinet_stream(const std::string &ARGV0, const uint64_t &MEM, const s
 
     MDB_cursor *cursor;
     if (!(retCode = mdb_cursor_open(txn, dbi, &cursor))) {
-      uint64_t entries{0};
       int32_t oldPercentage{-1};
       MDB_val key;
       MDB_val val;
 
+      if (START_IN_NS > 0) {
+        const uint64_t MAXKEYSIZE = 511;
+        std::vector<char> _key;
+        _key.reserve(MAXKEYSIZE);
+
+        cabinet::Key query;
+        query.timeStamp(START_IN_NS);
+        
+        key.mv_size = setKey(query, _key.data(), _key.capacity());
+        key.mv_data = _key.data();
+
+        if (!checkErrorCode(mdb_cursor_get(cursor, &key, &val, MDB_SET_RANGE), __LINE__, "mdb_cursor_get")) {
+          return retCode;
+        }
+      }
+
+      cabinet::Key storedKey;
       while ((retCode = mdb_cursor_get(cursor, &key, &val, MDB_NEXT_NODUP)) == 0) {
+        bool print{mapOfEnvelopesToExport.size() == 0};
+
+        const char *ptr = static_cast<char*>(key.mv_data);
+        storedKey = getKey(ptr, key.mv_size);
+
+        // Out of range.
+        if ( (END_IN_NS > 0) && (storedKey.timeStamp() > END_IN_NS) ) {
+          break;
+        }
+
         if (VERBOSE) {
-          const char *ptr = static_cast<char*>(key.mv_data);
-          cabinet::Key storedKey = getKey(ptr, key.mv_size);
           std::cerr << storedKey.timeStamp() << ": " << storedKey.dataType() << "/" << storedKey.senderStamp() << std::endl;
         }
 
-        //recFile.write(static_cast<char*>(val.mv_data), val.mv_size);
-        std::cout.write(static_cast<char*>(val.mv_data), val.mv_size);
-        std::cout.flush();
-        entries++;
+        if (mapOfEnvelopesToExport.size() > 0) {
+          std::stringstream sstr;
+          sstr << storedKey.dataType() << "/" << storedKey.senderStamp();
+          std::string str = sstr.str();
+          print = (mapOfEnvelopesToExport.count(str) > 0);
+        }
 
-        const int32_t percentage = static_cast<int32_t>(static_cast<float>(entries * 100.0f) / static_cast<float>(numberOfEntries));
-        if (((percentage % 5 == 0) && (percentage != oldPercentage)) && VERBOSE) {
-          std::cerr << "[" << ARGV0 << "]: Processed " << percentage << "% (" << entries << " entries) from " << CABINET << "." << std::endl;
-          oldPercentage = percentage;
+        if (print) {
+          if (storedKey.length() > val.mv_size) {
+            std::vector<char> decompressedValue;
+            decompressedValue.reserve(storedKey.length());
+            const ssize_t decompressedSize = LZ4_decompress_safe(static_cast<char*>(val.mv_data), decompressedValue.data(), val.mv_size, decompressedValue.capacity());
+            std::cout.write(static_cast<char*>(decompressedValue.data()), decompressedSize);
+          }
+          else {
+            // Value to dump is uncompressed.
+            std::cout.write(static_cast<char*>(val.mv_data), val.mv_size);
+          }
+          std::cout.flush();
+          entries++;
+        }
+
+        if (START == 0) {
+          const int32_t percentage = static_cast<int32_t>(static_cast<float>(entries * 100.0f) / static_cast<float>(numberOfEntries));
+          if (((percentage % 5 == 0) && (percentage != oldPercentage)) && VERBOSE) {
+            std::cerr << "[" << ARGV0 << "]: Processed " << percentage << "% (" << entries << " entries) from " << CABINET << "." << std::endl;
+            oldPercentage = percentage;
+          }
         }
       }
       mdb_cursor_close(cursor);
@@ -122,6 +171,7 @@ inline int cabinet_stream(const std::string &ARGV0, const uint64_t &MEM, const s
   if (env) {
     mdb_env_close(env);
   }
+  std::cerr << "[" << ARGV0 << "]: Exported " << entries << " entries from " << CABINET << "." << std::endl;
   return retCode;
 }
 
