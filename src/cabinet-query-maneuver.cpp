02a65d04cad57499e8120e6215d066037ba11171
fatfrog9 <l.birkemeyer@tu-bs.de>
2022-10-03 20:09:03 +0200

    add maneuver detection first level brute force

--- src/cabinet-query-maneuver.cpp
+++ src/cabinet-query-maneuver.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021  Christian Berger
+ * Copyright (C) 2022  Lukas Birkemeyer
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,6 +8,7 @@
 
 #include "cluon-complete.hpp"
 #include "cabinet-query-maneuver.hpp"
+#include "DrivingStatus.hpp"
 
 #include <cstdio>
 #include <cstring>
@@ -16,39 +17,6 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-
-
-class DrivingStatus {
-  public:
-    char* name;
-    std::pair<float,float> fenceBL;
-    std::pair<float,float> fenceTR;
-    uint64_t minDuration;
-    uint64_t maxDuration;
-    int64_t minGap; // to following;
-    int64_t maxGap; // to following;
-    int64_t minDiffTime; //how sensitive on single outliers
-    std::vector<uint64_t> relevantMorton;
-    std::vector<std::pair<int64_t,int64_t>> singleManeuverList;
-
-    DrivingStatus(char *name,
-                  std::pair<float,float> fenceBL,
-                  std::pair<float,float> fenceTR,
-                  uint64_t minDuration,
-                  uint64_t maxDuration,
-                  int64_t minGap,
-                  int64_t maxGap,
-                  int64_t minDiffTime)
-                  : name(name),
-                  fenceBL(fenceBL),
-                  fenceTR(fenceTR),
-                  minDuration(minDuration),
-                  maxDuration(maxDuration),
-                  minGap(minGap),
-                  maxGap(maxGap),
-                  minDiffTime(minDiffTime) {}
-};
-
 
 
 int64_t maneuverDetectorRecursiv(std::vector<DrivingStatus*> maneuver, int maneuver_idx, int status_idx) {
