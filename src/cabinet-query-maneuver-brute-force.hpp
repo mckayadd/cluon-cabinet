6dac4fab0d86ccb6132dba5a3ed9e0d0b686b1b6
fatfrog9 <l.birkemeyer@tu-bs.de>
2022-10-04 11:52:55 +0200

    update search process brute force

--- src/cabinet-query-maneuver-brute-force.hpp
+++ src/cabinet-query-maneuver-brute-force.hpp
@@ -32,13 +32,17 @@ inline bool in_fence(const std::pair<float,float> _fenceBL, const std::pair<floa
 
 inline int64_t recursiveManeuverDetector(const int64_t _ts, const int _dsID, std::vector<DrivingStatus*> _maneuver) {
 
-  if(_dsID >= maneuver.size())
+  if(_dsID >= _maneuver.size())
     return _ts;
 
-  int64_t _currTS = _ts - maneuver[_dsID]->minGap;
-  int64_t _maxTS = _ts - maneuver[_dsID]->maxGap;
+  int64_t _currTS = _ts - _maneuver[_dsID]->minGap;
+  int64_t _maxTS = _ts - _maneuver[_dsID]->maxGap;
 
-  
+  return 0;
+}
+
+
+inline int64_t rManeuverDetector() {
 
   return 0;
 }
@@ -50,38 +54,88 @@ inline bool cabinet_queryManeuverBruteForce(const uint64_t &MEM, const std::stri
     std::pair<float,float> _fenceBL;
     std::pair<float,float> _fenceTR;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    _fenceBL.first = -1.5; _fenceBL.second = 0.75;
-    _fenceTR.first = 0.75; _fenceTR.second = 5;
-    DrivingStatus *leftCurve  = new DrivingStatus( "leftCurve",
-            _fenceBL,
-            _fenceTR,
-            500000000,
-            3000000000,
-            -200000000,
-            2000000000,
-            160000000);
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      //_fenceBL.first = -1.5; _fenceBL.second = 0.75;
+      //_fenceTR.first = 0.75; _fenceTR.second = 5;
+      _fenceBL.first = 2; _fenceBL.second = -1;
+      _fenceTR.first = 8; _fenceTR.second = 1;
+      DrivingStatus *leftCurve  = new DrivingStatus( "leftCurve",
+              _fenceBL,
+              _fenceTR,
+              500000000,
+              3000000000,
+              -200000000,
+              2000000000,
+              160000000);
 
-    _fenceBL.first = -1.5; _fenceBL.second = -5;
-    _fenceTR.first = 0.75; _fenceTR.second = -0.75;
-    DrivingStatus *rightCurve = new DrivingStatus( "rightCurve",
-            _fenceBL,
-            _fenceTR,
-            500000000,
-            3000000000,
-            -200000000,
-            2000000000,
-            160000000);
+      _fenceBL.first = -1.5; _fenceBL.second = -5;
+      _fenceTR.first = 0.75; _fenceTR.second = -0.75;
+      DrivingStatus *rightCurve = new DrivingStatus( "rightCurve",
+              _fenceBL,
+              _fenceTR,
+              500000000,
+              3000000000,
+              -200000000,
+              2000000000,
+              160000000);
 
-    std::vector<DrivingStatus*> maneuver;
-    
-    maneuver.push_back(leftCurve);
-    maneuver.push_back(rightCurve);
+      std::vector<DrivingStatus*> maneuver;
+      
+      maneuver.push_back(leftCurve);
+      maneuver.push_back(rightCurve);
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     uint16_t _ID = APLX ? opendlv::device::gps::pos::Grp1Data::ID() : opendlv::proxy::AccelerationReading::ID();
+
+    auto env = lmdb::env::create();
+    env.set_mapsize(MEM * 1024UL * 1024UL * 1024UL);
+    env.set_max_dbs(100);
+    env.open(CABINET.c_str(), MDB_NOSUBDIR, 0600);
+
+    // Fetch key/value pairs in a read-only transaction.
+    auto rotxn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
+    auto dbiAll = lmdb::dbi::open(rotxn, "all");
+    dbiAll.set_compare(rotxn, &compareKeys);
+    std::clog << "Found " << dbiAll.size(rotxn) << " entries in database 'all'." << std::endl;
+
+
+    std::string DB = APLX ? "533/0" : "1030/2";
+    auto dbi = lmdb::dbi::open(rotxn, DB.c_str());
+    dbi.set_compare(rotxn, &compareKeys);
+    const uint64_t totalEntries = dbi.size(rotxn);
+    std::clog << "Found " << totalEntries << " entries in database '" << DB << "'." << std::endl;
+
+    auto cursor = lmdb::cursor::open(rotxn, dbi);
+    
+    bool flag = false;
+    int64_t start_TS = 0;
+    int64_t lastInFence_TS = 0;
+    float lastInFence_accelLon = 0.0;
+    float lastInFence_accelLat = 0.0;
+    
+    std::vector<std::pair<int64_t, int64_t>> maneuverDetectedList;
+
+
+    MDB_val key;
+    MDB_val value;
+    int32_t oldPercentage{-1};
+    uint64_t entries{0};
+    
+    cursor.close();
+    rotxn.abort();
+  }
+
+  catch (...) {
+    failed = true;
+  }
+  return failed;
+}
+
+#endif
+
+
+    /* uint16_t _ID = APLX ? opendlv::device::gps::pos::Grp1Data::ID() : opendlv::proxy::AccelerationReading::ID();
     uint16_t _subID = APLX ? 0 : 2;
     //uint16_t _ID = opendlv::proxy::AccelerationReading::ID();
     auto env = lmdb::env::create();
@@ -116,13 +170,22 @@ inline bool cabinet_queryManeuverBruteForce(const uint64_t &MEM, const std::stri
     MDB_val key;
     MDB_val value;
 
-    while (cursor.get(&key, &value, MDB_NEXT)) {
+    while (mdb_cursor_get(cursor, &key, &value, MDB_NEXT_NODUP) == 0) { //cursor.get(&key, &value, MDB_NEXT)
       entries++;
       MDB_val keyAll;
       MDB_val valueAll;
       keyAll = key;
       const char *ptr2 = static_cast<char*>(key.mv_data);
       cabinet::Key storedKey2 = getKey(ptr2, key.mv_size);
+
+      const uint64_t MAXKEYSIZE = 511;
+      std::vector<char> _key;
+      _key.reserve(MAXKEYSIZE);
+
+      storedKey2.timeStamp(1646666992460496002);
+
+      key.mv_size = setKey(storedKey2, _key.data(), _key.capacity());
+      key.mv_data = _key.data();
 
       if (lmdb::dbi_get(rotxn, dbiAll, &keyAll, &valueAll)) {
         const char *ptr = static_cast<char*>(keyAll.mv_data);
@@ -180,7 +243,7 @@ inline bool cabinet_queryManeuverBruteForce(const uint64_t &MEM, const std::stri
                     // hier den nächsten Detektor
                     int64_t end_TS = 0; // detector aufruf
 
-                    end_TS = recursiveManeuverDetector(lastInFence_TS, 1, maneuver);
+                    // end_TS = recursiveManeuverDetector(lastInFence_TS, 1, maneuver);
 
                     if(end_TS != 0){
                       // maneuver speichern (start, end)
@@ -200,6 +263,9 @@ inline bool cabinet_queryManeuverBruteForce(const uint64_t &MEM, const std::stri
       }
     }
 
+    for(auto _temp : maneuverDetectedList) {
+      std::cout << "Maneuver detected at: " << _temp.first << ", " << _temp.second << std::endl;
+    }
     
         
 
@@ -303,7 +369,7 @@ inline bool cabinet_queryManeuverBruteForce(const uint64_t &MEM, const std::stri
         oldPercentage = percentage;
       }
     } */
-
+/*
     std::clog << "number of entries " << entries << std::endl;
   
 
@@ -319,6 +385,4 @@ inline bool cabinet_queryManeuverBruteForce(const uint64_t &MEM, const std::stri
 
 inline void store_data() {
 
-}
-
-#endif
+} */
